6feb532121cced3fe67a07f1e2afe430346ea408
bytevoker <amanpandey76265@gmail.com>
2026-08-18 16:58:40 +0545

    Program Number 8

---
+++ rectangularpattern.py
@@ -0,0 +1,16 @@
+'''Program to print the following pattern:
+
+*****
+*   *
+*   *
+*   *
+*****
+
+'''
+num = int(input("Enter a number: "))
+
+for i in range(1,num+1):
+    if i == 1 or i == num:
+        print("*"*num)
+    else:
+        print("*" + " "*(num-2) + "*")
